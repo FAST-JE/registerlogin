8b427c0bce34a375447311eba8553082cf9c5f81
FAST <fast-jag@yandex.ru>
2020-02-14 11:25:43 +0300

    add password validation methods

--- app/test-doc-fast.loc/class/Validation.php
+++ app/test-doc-fast.loc/class/Validation.php
@@ -13,8 +13,33 @@ class Validation
         return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
+    /*
+     * @param string $input
+     *
+     * @return bool
+     */
     public function validateEmptyInput($input)
     {
-        return empty($input) ? false : true;
+        return !empty($input) ?? false;
+    }
+
+    /*
+     * @param string $password
+     *
+     * @return bool
+     */
+    public function validateLengthPassword($password)
+    {
+        return strlen($password) > 6 ?? false;
+    }
+
+    /*
+     * @param string $password1, $password2
+     *
+     * @return bool
+     */
+    public function validateEqualPasswords($password1, $password2)
+    {
+        return $password1 === $password2 ?? false;
     }
 }
