a0f8710fca02fd8384818fb517824faa29a286bc
FAST <fast-jag@yandex.ru>
2020-02-16 16:35:58 +0300

    separate form and handler

--- app/test-doc-fast.loc/index.php
+++ app/test-doc-fast.loc/index.php
@@ -26,71 +26,75 @@ require BASE_DIR . DS . 'class' . DS . 'Flash.php';
 //if (isset($_POST['func'])) {
 //    echo 'func exist';
 //}
-$errors = [];
+//$errors = [];
 
 //print_r($_POST);
 
 $db = DB::getInstance();
 
 $user = new User();
-$valid = new Validation();
+//$valid = new Validation();
 
-if ($_POST['func'] == 'register') {
-    $valid->validateData(
-        $_POST,
-        [
-            'email' => 'required, email',
-            'password' => 'required, min:5'
-        ]
-    );
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
-    try {
-        if (empty($valid->errors)) {
-            $user->register($email, $pass);
-        }
-    } catch (Exception $e) {
-        array_push($errors, $e->getMessage());
-    }
+//if ($_POST['func'] == 'register') {
+//    $valid->validateData(
+//        $_POST,
+//        [
+//            'email' => 'required, email',
+//            'password' => 'required, min:5'
+//        ]
+//    );
+//    $email = $_POST['email'];
+//    $pass = $_POST['password'];
+//    try {
+//        if (empty($valid->errors)) {
+//            $user->register($email, $pass);
+//        }
+//    } catch (Exception $e) {
+//        array_push($errors, $e->getMessage());
+//    }
+//
+//    Flash::setWarning($valid->errors[0] ?? $errors[0]);
+//    header("Location: /index.php");
+//}
+//if ($_POST['func'] == 'login') {
+//    $valid->validateData(
+//        $_POST,
+//        [
+//            'email' => 'required, email',
+//            'password' => 'required'
+//        ]
+//    );
+//    $email = $_POST['email'];
+//    $pass = $_POST['password'];
+//    try {
+//        if (empty($valid->errors)) {
+//            $user->login($email, $pass);
+//        }
+//    } catch (Exception $e) {
+//        array_push($errors, $e->getMessage());
+//    }
+//    Flash::setWarning($valid->errors[0] ?? $errors[0]);
+//    header("Location: /index.php");
+//}
+//if ($_POST['func'] == 'logout') {
+//    $user->logout();
+//}
 
-    Flash::setWarning($valid->errors[0] ?? $errors[0]);
-    header("Location: /index.php");
-}
-if ($_POST['func'] == 'login') {
-    $valid->validateData(
-        $_POST,
-        [
-            'email' => 'required, email',
-            'password' => 'required'
-        ]
-    );
-    $email = $_POST['email'];
-    $pass = $_POST['password'];
-    try {
-        if (empty($valid->errors)) {
-            $user->login($email, $pass);
-        }
-    } catch (Exception $e) {
-        array_push($errors, $e->getMessage());
-    }
-    Flash::setWarning($valid->errors[0] ?? $errors[0]);
-    header("Location: /index.php");
-}
-if ($_POST['func'] == 'logout') {
-    $user->logout();
-}
+//if ($_POST['func'] != 'register' && $_POST['func'] != 'login') {
+//    if (Flash::getMessageType() == 3) {
+//        echo "<h1 style='color: red;'>".Flash::getMessageText()."</h1>";
+//    }
+//}
 
-if ($_POST['func'] != 'register' && $_POST['func'] != 'login') {
-    if (Flash::getMessageType() == 3) {
-        echo "<h1 style='color: red;'>".Flash::getMessageText()."</h1>";
-    }
+if (Flash::getMessageType() == 3) {
+    echo "<h1 style='color: red;'>".Flash::getMessageText()."</h1>";
 }
 
 
 ?>
 <? if (!$user->isLogged()) { ?>
 <h1>Register</h1>
-<form action="" method="POST">
+<form action="/handler.php" method="POST">
     <input type="email" name="email" placeholder="email">
     <input type="password" name="password" placeholder="password">
     <input type="hidden" name="func" value="register">
@@ -98,7 +102,7 @@ if ($_POST['func'] != 'register' && $_POST['func'] != 'login') {
 </form>
 <hr>
 <h1>Вход</h1>
-<form action="" method="POST">
+<form action="/handler.php" method="POST">
     <input type="email" name="email" placeholder="email">
     <input type="password" name="password" placeholder="password">
     <input type="hidden" name="func" value="login">
@@ -107,7 +111,7 @@ if ($_POST['func'] != 'register' && $_POST['func'] != 'login') {
 <? } ?>
 <? if ($user->isLogged()) { ?>
     <h1>logged</h1>
-    <form action="" method="POST">
+    <form action="/handler.php" method="POST">
         <input type="hidden" name="func" value="logout">
         <input type="submit" value="logout">
     </form>
